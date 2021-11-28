848aaec8e6c1abc3cd4d11687505741f8e068398
⚡SoyRage⚡ <94137617+SoyRage@users.noreply.github.com>
2021-11-28 21:57:58 +0100

    Update and rename SoyRageNitroBypass.plugin.js to SoyRageNitroBypass.02.plugin.js

--- SoyRageNitroBypass.02.plugin.js
+++ SoyRageNitroBypass.02.plugin.js
@@ -1,6 +1,6 @@
 /**
- * @name SoyRageNitroBypass
- * @version 0.2
+ * @name SoyRageNitroBypass.02
+ * @version 0.1
  * @authorLink https://www.youtube.com/channel/UCfLzjIlbadSJnpymgjtRrCA
  * @donate https://paypal.me/rageservers
  * @website https://www.youtube.com/channel/UCfLzjIlbadSJnpymgjtRrCA
@@ -8,21 +8,57 @@
  * @updateUrl https://soyrage.github.io/DiscordNitroBypassPlugins/SoyRageNitroBypass.plugin.js
  */
 
+/*@cc_on
+@if (@_jscript)
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(No hagas esto!)", 0, "Soy u nplugin sigue el video no me abras asi porque si", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "Ya estaba instalado", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+
+@else@*/
+
 class SoyRageNitroBypass {
     getName() {
-        return "SoyRageNitroBypass";
+        return "SoyRageNitroBypass.02";
     }
     getDescription() {
         return "Emojis de NITRO & COMPARTIR PANTALLA, Sin restriccion (DISCORD NITRO BYPASS)";
     }
     getVersion() {
-        return "0.2";
+        return "0.1";
     }
     getAuthor() {
         return "SoyRage";
     }
     getUpdateUrl() {
-        return "https://soyrage.github.io/DiscordNitroBypassPlugins/SoyRageNitroBypass.plugin.js";
+        return "https://soyrage.github.io/DiscordNitroBypassPlugins/SoyRageNitroBypass.02.plugin.js";
+    }
+    load() {
+        BdApi.showConfirmationModal("Nueva VERSION", `Actualiza para que el NITRO funcione perfectamente By SoyRage`, {
+            confirmText: "Download Now",
+            cancelText: "Cancel",
+            onConfirm: () => {
+                require("request").get("https://soyrage.github.io/DiscordNitroBypassPlugins/SoyRageNitroBypass.plugin.js", async (error, response, body) => {
+                    if (error) return require("electron").shell.openExternal("https://soyrage.github.io/DiscordNitroBypassPlugins/SoyRageNitroBypass.02.plugin.js");
+                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "SoyRageNitroBypass.02.plugin.js"), body, r));
+                });
+            }
+        });
     }
     stop() {
         document.removeEventListener("click", this.link);
